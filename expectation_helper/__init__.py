895eee02a59fd00a0dadc1629e3835b7ccecac72
Ken <kenjyco@gmail.com>
2023-12-13 05:41:49 -0600

    Use _gx_local_repo_path var

--- expectation_helper/__init__.py
+++ expectation_helper/__init__.py
@@ -6,12 +6,12 @@ from os.path import isdir
 
 SETTINGS = sh.get_all_settings(__name__).get(sh.APP_ENV, {})
 
-gx_local_repo_path = fh.abspath(SETTINGS.get('gx_local_repo_path'))
-assert gx_local_repo_path, 'The GX_LOCAL_REPO_PATH is not set'
-if not isdir(gx_local_repo_path):
-    print(f'Cloning great_expectations to {gx_local_repo_path}')
+_gx_local_repo_path = fh.abspath(SETTINGS.get('gx_local_repo_path'))
+assert _gx_local_repo_path, 'The GX_LOCAL_REPO_PATH is not set'
+if not isdir(_gx_local_repo_path):
+    print(f'Cloning great_expectations to {_gx_local_repo_path}')
     bh.tools.git_clone(
         'https://github.com/great-expectations/great_expectations',
-        gx_local_repo_path,
+        _gx_local_repo_path,
         show=True
     )
